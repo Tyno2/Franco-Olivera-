57126acf354481e24966d86394227036f41bcd4c
Ariel Nicosia <92611797+Arieln123@users.noreply.github.com>
2024-04-23 22:34:05 -0300

    Pruebas
    login/registro/contacto

---
+++ codo a codo prueba/formulario.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        e.stopPropagation();
+    });
+
+    $('html').click(function(){
+        $('.nav-dropdown').hide();
+    })
+    $('#nav-toggle').click(function(){
+        $('nav ul').slideToggle();
+    })
+    $('#nav-toggle').on('click', function(){
+        this.classList.toggle('active');
+    });
+});
+
+
+//////form
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+  var object = {};
+  formData.forEach((value, key) => {
+    object[key] = value;
+  });
+  var json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: json
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
---
+++ codo a codo prueba/registro.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function() {
+  $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      e.stopPropagation();
+  });
+
+  $('html').click(function(){
+      $('.nav-dropdown').hide();
+  })
+  $('#nav-toggle').click(function(){
+      $('nav ul').slideToggle();
+  })
+  $('#nav-toggle').on('click', function(){
+      this.classList.toggle('active');
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//////registro
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+    var $this = $(this),
+        label = $this.prev('label');
+  
+        if (e.type === 'keyup') {
+              if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+          if( $this.val() === '' ) {
+              label.removeClass('active highlight'); 
+              } else {
+              label.removeClass('highlight');   
+              }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+              label.removeClass('highlight'); 
+              } 
+        else if( $this.val() !== '' ) {
+              label.addClass('highlight');
+              }
+      }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+  
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
